a601fb70d8eefdc4f3d1c1270d99d4f3d39ca955
ArifurSajid <arifursajid3456@gmail.com>
2025-11-24 10:49:29 +0600

    QuienesSomosBanner update responsive

---
+++ src/components/quienessomos/QuienesSomosBanner/QuienesSomosBanner.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import QuienesSomosImg from "../../../assets/Grupo.png";
+import "./QuienesSomosBanner.css"; // import custom CSS
+
+const QuienesSomosBanner: React.FC = () => {
+  return (
+    <div className="w-[1920px] h-[555.7px] bg-gradient-to-r from-green-700 to-orange-300 relative overflow-hidden flex items-center px-16 quienes-banner">
+      {/* Background Image */}
+      <img
+        src={QuienesSomosImg}
+        alt="quienes somos"
+        className="w-[1004.94px] h-[546.37px] absolute right-0 top-0 object-contain pointer-events-none quienes-img"
+      />
+
+      {/* Title */}
+      <h1 className="text-white font-bold uppercase z-10 leading-[148px] text-left tracking-[0px] text-[123px] font-['AvenirNextLTPro'] quienes-title">
+        QUIÉNES SOMOS
+      </h1>
+    </div>
+  );
+};
+
+export default QuienesSomosBanner;
